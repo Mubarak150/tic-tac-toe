e5cfcd1e9548f2032b9edff3b5ac0ec54169b6db
Mubarak150 <mkmubarak2347@gmail.com>
2025-03-15 03:36:21 +0500

    play against computer feature added

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ let boxes = document.querySelectorAll(".box"); // -------------- all the input b
 let resetBtn = document.querySelector("#reset-button"); // ----- reset/newgame button
 let turnTag = document.querySelector(".turn"); // -------------- text showing whose turn is it or who won?
 
+// ------------------------------------------------------------- CHOOSING POP UP COMPONENTS
+let chooseTypePopupContainer = document.querySelector(".choose-type-popup");
+let chooseTypePopup = document.querySelector(".choose-type-popup-card");
+let chooseTypePopupText = document.querySelector(".choose-type-popup-text");
+let playComputerBtn = document.querySelector("#play-against-computer");
+let playDualBtn = document.querySelector("#choose-type-dual");
+
+let showChooseTypePopup = true;
+let opponentIsComputer = false;
+let opponentIsDual = false;
+// ------------------------------------------------------------- CHOOSING POP UP COMPONENTS [END]
 // ------------------------------------------------------------- WINNING POP UP COMPONENTS
 let popupContainer = document.querySelector(".popup");
 let popup = document.querySelector(".popup-card");
 let popupText = document.querySelector(".popup-text");
-let newGameBtn = document.querySelector("#newGame");
+let playAgainstComputerBtn = document.querySelector("#choose-type-computer");
+let playDualBtn2 = document.querySelector("#two-players");
 let hidePopupBtn = document.querySelector("#hidePopup");
 
 let showPopup = false;
@@ -42,6 +54,48 @@ const winPatterns = [
   [2, 4, 6], // ------------------------------------------------- diagonal win
 ];
 
+/*
+first of all let the user choose from 2 available formats... 
+1. if he choose against computer: 
+  i.   let computer play O i.e. the first. so player O is computer. and since we are providing already for  O first approach so all sorted in tihs step. 
+  ii.  contain two arrays that tracks the moves of user and that of the computer. 
+  iii. the computer should first check for any wins. 
+  iv.  if no wins then try to block any possible win of user. 
+  v.   if no wins then attack a corner, 
+  vi.  if no corner left empty then attack the center. 
+  v.   if no center then place a random move. 
+  vi.  return -1 if no boxes left empty. 
+  vii. switch between user and computer. 
+
+*/
+// PLAY AGAINST COMPUTER.
+
+let playComputer = () => {
+  opponentIsComputer = true;
+  opponentIsDual = false;
+  chooseTypePopupContainer.style.display = "none";
+  popupContainer.style.display = "none";
+  darkBtn.disabled = false;
+  restartGame();
+  chooseBoxByComputer();
+};
+
+let playDual = () => {
+  opponentIsDual = true;
+  opponentIsComputer = false;
+  chooseTypePopupContainer.style.display = "none";
+  popupContainer.style.display = "none";
+  darkBtn.disabled = false;
+  restartGame();
+};
+
+playComputerBtn.addEventListener("click", playComputer);
+playAgainstComputerBtn.addEventListener("click", playComputer);
+
+playDualBtn.addEventListener("click", playDual);
+playDualBtn2.addEventListener("click", playDual);
+
+//  DUAL PLAY CODE.
 // --------------------------------------------------------------- if one wins.
 // --------------------------------------------------------------- then announce the winner, highlight the win pattern, disable all the boxes to avoid further entries, and change reset to newgame.
 const handleWin = (pattern, player) => {
@@ -78,7 +132,7 @@ const restartGame = () => {
 };
 
 // ---------------------------------------------------------------- check if there is a winner.
-const checkWinner = () => {
+const checkWinner = (opponent = "dual") => {
   // -------------------------------------------------------------- look in all the winPatternsf any one satisfies?
   for (let pattern of winPatterns) {
     let position1 = boxes[pattern[0]];
@@ -97,33 +151,100 @@ const checkWinner = () => {
       }
     }
   }
+  // if the user is not a winner yet then check if computer, if yes then make a move:
+  if (opponent == "computer") {
+    turnTag.innerText = "computer is thinking";
+    setTimeout(chooseBoxByComputer, 700);
+  }
 };
 
 // ---------------------------------------------------------------- when a user clicks on one of .game > .boxes
-const handleEntry = (target) => {
+const handleEntry = (target, opponent = "dual") => {
   target.innerText = turnOfO ? "O" : "X";
   turnOfO = !turnOfO;
   turnTag.innerText = `${turnOfO ? "O" : "X"}'s turn now`; // -------- enter cross or O in the box and change the turn
 
   target.disabled = true; //---------------------------------------- and disable the button to avoid re-click
 
-  checkWinner(); //------------------------------------------------- then check if a pattern is matched.
+  checkWinner(opponent); //------------------------------------------------- then check if a pattern is matched.
 };
 
 // ------------------------------------------------------------------ boxes event handler... main part of the game....
 boxes.forEach((box) => {
   box.addEventListener("click", (e) => {
-    handleEntry(e.target);
+    if (opponentIsDual) {
+      handleEntry(e.target);
+    } else if (opponentIsComputer) {
+      if (!turnOfO) {
+        handleEntry(e.target, "computer");
+      }
+    }
   });
 });
 
-// -------------------------------------------------------------------  RESET, NEW GAME, and HIDE BUTTONS handler.
-resetBtn.addEventListener("click", restartGame);
+let chooseBoxByComputer = () => {
+  console.log("computer");
+  let marked = false;
+  let empties;
+  let myBoxes;
+  let opponentBoxes;
+  let completedPattern = [];
 
-newGameBtn.addEventListener("click", () => {
-  popupContainer.style.display = "none";
-  darkBtn.disabled = false;
-  restartGame();
+  // 1. tracking the win:
+  for (let pattern of winPatterns) {
+    myBoxes = pattern.filter((box) => boxes[box].innerText == "O");
+    opponentBoxes = pattern.filter((box) => boxes[box].innerText == "X");
+    if (myBoxes.length == 2 && opponentBoxes.length == 0) {
+      empties = pattern.filter((box) => boxes[box].innerText == "");
+      marked = true;
+      completedPattern = pattern;
+    }
+    if (empties) {
+      boxes[empties[0]].innerText = "O";
+    }
+  }
+
+  if (completedPattern.length > 0) {
+    handleWin(completedPattern, "O"); // computer i.e. O wins...
+  }
+
+  if (!marked) {
+    // 2. blocking any win of opponent..
+    for (let pattern of winPatterns) {
+      myBoxes = pattern.filter((box) => boxes[box].innerText == "O");
+      opponentBoxes = pattern.filter((box) => boxes[box].innerText == "X");
+      if (opponentBoxes.length == 2 && myBoxes.length == 0) {
+        empties = pattern.filter((box) => boxes[box].innerText == "");
+        marked = true;
+      }
+      if (empties) {
+        boxes[empties[0]].innerText = "O";
+      }
+    }
+  }
+
+  if (!marked) {
+    // 3. targetting empty corners ELSE taking the center.
+    let corners = [0, 2, 6, 8];
+    let emptyCorners = corners.filter((box) => boxes[box].innerText == "");
+    let randomCorner = Math.floor(Math.random() * emptyCorners.length);
+    // console.log(randomCorner);
+    if (emptyCorners.length > 0) {
+      marked = true;
+      boxes[emptyCorners[randomCorner]].innerText = "O";
+    } else if (boxes[5].innerText == "") {
+      marked = true;
+      boxes[5].innerText == "O"; // taking the center
+    }
+  }
+
+  turnOfO = !turnOfO;
+  turnTag.innerText = "your turn now";
+};
+
+// -------------------------------------------------------------------  RESET, NEW GAME, and HIDE BUTTONS handler.
+resetBtn.addEventListener("click", () => {
+  chooseTypePopupContainer.style.display = "flex";
 });
 
 hidePopupBtn.addEventListener("click", () => {
@@ -158,6 +279,12 @@ darkBtn.addEventListener("click", () => {
 
 /*
 MUHAMMAD MUBARAK AZAD ADVOCATE
+
+first deploy: 
 Room 289, Hostel 1, University of Peshawar, Pakistan. 
 Dated: March 10th, 2025 0508 AM [PST]
+
+play against computer added at: 
+Mohallah Miandad Khail, Aboha, Tehsil Barikot, Swat, Pakistan.
+Dated: March 15th, 2025 0255 AM [PST]
 */
